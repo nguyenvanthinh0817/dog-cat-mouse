1e34c86b96e097345bf1bd658512d08efed04c84
NguyenVanThinh <ducthinhlatao4s@gmail.com>
2020-05-07 13:35:22 +0700

    Let dog eat cat

--- dog.js
+++ dog.js
@@ -3,8 +3,8 @@ function Dog(name){
 	this.stomach = [];
 	this.name = name;
 };
-Dog.prototype.eat = function(){
-	this.stomachh.push(cat);
+Dog.prototype.eat = function(cat){
+	this.stomach.push(cat);
 };
 
 Dog.prototype.sayHi = function(){
--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 var Dog = require('./Dog');
 
-
+var Cat = require('./Cat');
 var dog = new Dog('Lu');
+
+var tom = new Cat();
+
+
+dog.eat(tom);
+console.log(dog)
 dog.sayHi();
